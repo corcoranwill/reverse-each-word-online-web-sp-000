cc9cdee1507361672442885c7216f40dca27956f
Will Corcoran <corcoranwill@gmail.com>
2020-02-16 20:12:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(sentence)
-
-
   sentence.split.collect {|word| word.reverse}.join(" ")
 
   # a = sentence.split
-  
+
   # a_rev_each = []
   # a.each {|x| a_rev_each << x.reverse}
   # words_rev_each = a_rev_each.join(" ")
